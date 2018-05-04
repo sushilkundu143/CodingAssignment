5ec21c17e993728c3728bef22bd4832b2dc93a88
sushilkundu143 <sushilkundu143@gmail.com>
2018-05-04 08:41:58 +0530

    add update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
-$('#submit').submit(function(event) {
-    //event.preventdefault();
-    console.log('submiting');
-});
-
-function validate(input){
-    var val = input.val();
-    if(input.attr('type') == 'email'){
-        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-        return pattern.test(emailText);
+$.validator.setDefaults({
+    submitHandler: function() {
+        alert("submitted!");
     }
-}
+});
+jQuery.validator.addMethod("lettersonly", function(value, element) {
+    return this.optional(element) || /^[a-z]+$/i.test(value);
+  }, "Letters only please"); 
+$().ready(function() {
+    // validate the comment form when it is submitted
+    $("#form").validate({
+        rules: {
+            name: {
+             required: true,
+             minlength: 3,
+             lettersonly: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            number: {
+                required: true,
+                minlength: 10
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter your name",
+                minlength: "Name should at least 3 character long",
+                lettersonly: "Please enter character only."
+            },
+            email: {
+                required: "Please enter your email address",
+                email: "Please enter a valid email address"
+            },
+            number: {
+                required: "Please enter your phone number",
+                minlength: "Phone number should contain 10 digits."
+            } 
+        }
+    });
+
+});
